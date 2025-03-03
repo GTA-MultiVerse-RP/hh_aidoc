57df34bf7213bc836fc6769dab549136a5640a60
gta multiverse rp <gta-multiverse-rp@fx.server>
2025-03-03 17:54:01 +0900

    導入

---
+++ config.lua
@@ -0,0 +1,9 @@
+-------------------
+-- CONFIG --
+-------------------
+Config = {}
+
+Config.Doctor = 0 -- Minimum Amount of EMS to work
+Config.Price = 1500000
+Config.ReviveTime = 200000  --in msec
+
